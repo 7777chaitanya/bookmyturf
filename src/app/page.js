77995b422532a480a08a7cb5d483e8652528185a
Chaitanya7666 <chaitu.y25@gmail.com>
2024-04-14 16:24:52 +0530

    pwa

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,14 @@
 'use client'
-import Image from "next/image";
-import styles from "./page.module.css";
-import {useState} from 'react'
+// import Image from "next/image";
+// import styles from "./page.module.css";
+import {useState, useEffect} from 'react'
 
 export default function Home() {
   const [name, setName] = useState('');
+  
+  // useEffect(() => {
+  //   navigator.serviceWorker.register('/serviceWorker.js')
+  // }, []);
   return (
     <div>
       <input type="text" onChange={(e)=>{setName(e.target.value)}}/>
